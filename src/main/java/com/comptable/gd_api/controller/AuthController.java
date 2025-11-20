1582348c2c5989113f42e9ffc106ebaf80c30211
Aymane Boujeddi <aymane655@gmail.com>
2025-11-20 18:51:55 +0100

    add AuthController to handle user login requests and authenticate using AuthService

---
+++ src/main/java/com/comptable/gd_api/controller/AuthController.java
@@ -0,0 +1,26 @@
+package com.comptable.gd_api.controller;
+
+import com.comptable.gd_api.dto.request.AuthRequestDTO;
+import com.comptable.gd_api.dto.response.AuthResponseDTO;
+import com.comptable.gd_api.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponseDTO> loginUser(@Valid @RequestBody AuthRequestDTO authRequestDTO) {
+        AuthResponseDTO response = authService.authenticate(authRequestDTO);
+        return ResponseEntity.ok(response);
+    }
+}
